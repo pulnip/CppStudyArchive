0d53b87a3e2221a35ad83dd8a2ace0a9e7eea104
pulnip <choiwh1008@gmail.com>
2024-09-26 01:10:31 +0900

    update screenshot.cpp

--- src/cvtest/screenshot.cpp
+++ src/cvtest/screenshot.cpp
@@ -23,35 +23,46 @@ class hwnd2Mat{
     virtual void read();
 };
 
+BITMAPINFOHEADER createBitmapHeader(int width, int height){
+    // http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+    return BITMAPINFOHEADER{
+        .biSize=sizeof(BITMAPINFOHEADER),
+        .biWidth=width,
+        // draw upside down or not
+        .biHeight=-height,
+        .biPlanes=1,
+        .biBitCount=32,
+        .biCompression=BI_RGB,
+        .biSizeImage=0,
+        .biXPelsPerMeter=0,
+        .biYPelsPerMeter=0,
+        .biClrUsed=0,
+        .biClrImportant=0
+    };
+}
+
 hwnd2Mat::hwnd2Mat(HWND hwindow, float scale){
-    hwnd = hwindow;
-    hwindowDC = GetDC(hwnd);
-    hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+    // get handles to a device context
+    hwnd=hwindow;
+    hwindowDC=GetDC(hwnd);
+    hwindowCompatibleDC=CreateCompatibleDC(hwindowDC);
     SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
 
-    RECT windowsize;    // get the height and width of the screen
+    // get the height and width of the screen
+    RECT windowsize;
     GetClientRect(hwnd, &windowsize);
 
-    srcheight = windowsize.bottom;
-    srcwidth = windowsize.right;
-    height = (int)(windowsize.bottom * scale);
-    width = (int)(windowsize.right * scale);
+    srcheight=windowsize.bottom;
+    srcwidth=windowsize.right;
+    height=(int)(windowsize.bottom * scale);
+    width=(int)(windowsize.right * scale);
 
+    // create mat object
     image.create(height, width, CV_8UC4);
 
     // create a bitmap
-    hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
-    bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
-    bi.biWidth = width;
-    bi.biHeight = -height;  //this is the line that makes it draw upside down or not
-    bi.biPlanes = 1;
-    bi.biBitCount = 32;
-    bi.biCompression = BI_RGB;
-    bi.biSizeImage = 0;
-    bi.biXPelsPerMeter = 0;
-    bi.biYPelsPerMeter = 0;
-    bi.biClrUsed = 0;
-    bi.biClrImportant = 0;
+    hbwindow=CreateCompatibleBitmap(hwindowDC, width, height);
+    bi=createBitmapHeader(width, height);
 
     // use the previously created device context with the bitmap
     SelectObject(hwindowCompatibleDC, hbwindow);
@@ -60,8 +71,9 @@ hwnd2Mat::hwnd2Mat(HWND hwindow, float scale){
 
 void hwnd2Mat::read(){
     // copy from the window device context to the bitmap device context
-    StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
-    GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, image.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+    StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY);
+    // copy from hwindowCompatibleDC to hbwindow
+    GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, image.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
 };
 
 hwnd2Mat::~hwnd2Mat(){
@@ -73,29 +85,28 @@ hwnd2Mat::~hwnd2Mat(){
 
 int main(){
     Mat ui(40, 400, CV_8UC3, Scalar(0, 130, 0));
-    putText(ui, "Press Esc to stop capturing", Point(30,30), FONT_HERSHEY_COMPLEX, 0.7,
-        Scalar(0, 0, 255), 1);
+    putText(ui, "Press Esc to stop capturing", Point(30,30),
+        FONT_HERSHEY_COMPLEX, 0.7, Scalar(0, 0, 255), 1);
     Mat bgrImg;
     hwnd2Mat capDesktop(GetDesktopWindow());
 
     VideoWriter writer;
-    int codec = VideoWriter::fourcc('X', 'V', 'I', 'D');
-    double fps = 10.0;
-    string filename = "./desktop_capture.avi";
+    int codec=VideoWriter::fourcc('X', 'V', 'I', 'D');
+    double fps=10.0;
+    string filename="./desktop_capture.avi";
     writer.open(filename, codec, fps, capDesktop.image.size(), true);
     // check if we succeeded
-    if (!writer.isOpened()) {
-        cerr << "Could not open the output video file for write\n";
+    if(!writer.isOpened()){
+        cerr<<"Could not open the output video file for write\n";
         return -1;
     }
 
-    while ( true )
-    {
+    while(true){
         capDesktop.read();
         cvtColor(capDesktop.image, bgrImg, COLOR_BGRA2BGR);
         writer << bgrImg;
         imshow("desktop capture", ui);
-        int key = waitKey(5);
+        int key=waitKey(5);
 
         if(key == 27)
             break;
