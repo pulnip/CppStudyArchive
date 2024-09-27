9856efe12338836b406ff82dbc39f4e57dd005cf
pulnip <choiwh1008@gmail.com>
2024-09-27 18:23:25 +0900

    na hw2

--- include/natest/root_finding.h
+++ include/natest/root_finding.h
@@ -2,4 +2,5 @@
 
 extern void bisection(double(*f)(double), double a, double b, const double TOL, const int n0);
 extern void secant(double(*f)(double), double p0, double p1, const double TOL, const int n0);
+extern void secant_opt(double(*f)(double), double p0, double p1, const double TOL, const int n0);
 extern void newton(double(*fp0)(double), double(*fp1)(double), double x0, double x1, const double TOL, const int n0);
--- src/natest/root_finding.c
+++ src/natest/root_finding.c
@@ -7,6 +7,7 @@ void bisection(double(*f)(double), double a, double b,
     const double TOL, const int n0
 ){
     double fa=f(a);
+    double fb=f(b);
     int inc=fa<0;
 
     assert(a < b);
@@ -40,7 +41,9 @@ void secant(double(*f)(double), double p0, double p1,
     assert(TOL > 0);
     assert(n0 > 0);
 
+#ifdef SHOW_INTERNAL_PROCESS
     puts("_n|______________pn|___________f(pn)");
+#endif
     for(int i=2; i<n0; ++i){
         if(fabs(p1-p0) < TOL)
             break;
@@ -48,11 +51,45 @@ void secant(double(*f)(double), double p0, double p1,
         double fp0=f(p0), fp1=f(p1);
         double p=(p0*fp1 - p1*fp0)/(fp1-fp0);
         double fp=f(p);
+#ifdef SHOW_INTERNAL_PROCESS
         printf("%2d|%16.10lf|%16.10lf\n", i, p, fp);
+#endif
 
         if(fp == 0)
             break;
 
+        p0=p1;
+        p1=p;
+    }
+}
+
+void secant_opt(double(*f)(double), double p0, double p1,
+    const double TOL, const int n0
+){
+    assert(TOL > 0);
+    assert(n0 > 0);
+    assert(n0 < 100);
+
+    double fp[100]={f(p0), f(p1)};
+    double u[100]={p0*fp[0], p1*fp[1]};
+
+#ifdef SHOW_INTERNAL_PROCESS
+    puts("_n|______________pn|___________f(pn)");
+#endif
+    for(int i=2; i<n0; ++i){
+        if(fabs(p1-p0) < TOL)
+            break;
+
+        double p = p0 + p1 + (u[i-1]-u[i-2]) / (fp[i-1]-fp[i-2]);
+        double _fp=f(p);
+#ifdef SHOW_INTERNAL_PROCESS
+        printf("%2d|%16.10lf|%16.10lf\n", i, p, _fp);
+#endif
+        if(_fp == 0)
+            break;
+
+        fp[i]=_fp;
+        u[i]=p*_fp;
         p0=p1;
         p1=p;
     }
