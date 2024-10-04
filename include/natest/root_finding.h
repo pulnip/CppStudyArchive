24d17d86ec6f64c045dd25cdfce40d69c65222ff
pulnip <choiwh1008@gmail.com>
2024-10-05 01:23:24 +0900

    :building_construction: new start

--- include/natest/root_finding.h
+++
@@ -1,6 +0,0 @@
-#pragma once 
-
-extern void bisection(double(*f)(double), double a, double b, const double TOL, const int n0);
-extern void secant(double(*f)(double), double p0, double p1, const double TOL, const int n0);
-extern void secant_opt(double(*f)(double), double p0, double p1, const double TOL, const int n0);
-extern void newton(double(*fp0)(double), double(*fp1)(double), double x0, double x1, const double TOL, const int n0);
