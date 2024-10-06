adbd1cc81cd316a1a6b4241563f04c690dcaedcd
pulnip <choiwh1008@gmail.com>
2024-10-07 00:32:52 +0900

    :seedling: initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
-#include <exception>
-#include <print>
+#include"myTL/Set.h"
 
-using namespace std;
+#include<iostream>
+#include<string>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]){
-    try{
-        println("Hello, World!");
-    }
-    catch(const exception& e){
-        println("{}", e.what());
-    }
+int main(void){
+    My::Set<int> set;
 
-    return 0;
+    std::string cmd;
+    int arg=0;
+    while(true){
+        std::cout<<"current state of set"<<std::endl;
+        set.show_contents();
+        std::cout<<"--------------------"<<std::endl;
+        std::cout<<"command: ";
+        std::cin>>cmd;
+
+        if(!cmd.compare("quit")) break;
+        else{
+            std::cin>>arg;
+            if(!cmd.compare("insert")){
+                std::cout<<"Result: "<<std::boolalpha<<set.insert(arg)<<'\n';
+            }
+            else if(!cmd.compare("erase")){
+                std::cout<<"Result: "<<std::boolalpha<<set.erase(arg)<<'\n';
+            }
+            else if(!cmd.compare("count")){
+                std::cout<<"Result: "<<set.count(arg)<<'\n';
+            }
+            std::cout<<std::endl;
+        }
+    }    
 }
